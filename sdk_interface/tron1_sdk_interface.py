b44c570c03058313e6705349d1f8c17c4aa0cc4e
silvery107 <yulunz@umich.edu>
2025-04-17 19:49:33 -0400

    Refactor sdk interface for tron1
    - Support both point foot and line foot models
    - Add unified robot related constants
    - Use joint idx to motor idx map to avoid messy indexing
    - Use unified topic names

--- sdk_interface/tron1_sdk_interface.py
+++ sdk_interface/tron1_sdk_interface.py
@@ -16,14 +16,49 @@ import numpy as np
 # Add the parent directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from lcm_types.robot_lcm import tron1_pointfoot_state_t, tron1_pointfoot_control_t
+from lcm_types.robot_lcm import tron1_linefoot_state_t, tron1_linefoot_control_t
 from state_estimators import MovingWindowFilter
 from utils import *
 
 # TODO safety check and soft estop
+np.set_printoptions(precision=4, suppress=True, linewidth=1000)
+ROBOT_TYPE = "LF"  # PF or LF
 
+if ROBOT_TYPE == "PF":
+    # ========== Point Foot Robot Constants ==========
+    TOPIC_STATE = "tron1_pointfoot_state" # state to mujoco bridge
+    TOPIC_CONTROL = "tron1_pointfoot_control".upper() # control from MATLAB
 
-JOINT_OFFSETS_HIGH = np.array([0, 0.53 - 0.06, -0.55 - 0.54,  # right leg
-                                0, 0.53 - 0.06, -0.55 - 0.54]) # left leg
+    DEFAULT_JOINT_OFFSETS = np.array([0, 0.53 - 0.06, -0.55 - 0.54,  # right leg
+                                    0, 0.53 - 0.06, -0.55 - 0.54]) # left leg
+
+    DEFAULT_JOINT_POS = np.array([0.0, 0.25, -0.5,  
+                                0.0, 0.25, -0.5,])
+
+    JOINT_IDX_2_MOTOR_IDX = np.array([3, 4, 5, 
+                                    0, 1, 2])
+
+    MOTOR_POLARITY = np.array([1,  1, -1, 
+                            1, -1,  1])
+elif ROBOT_TYPE == "LF":
+    # ========== Line Foot Robot Constants ==========
+    ROBOT_TYPE = "LF"
+    TOPIC_STATE = "tron1_linefoot_state" # state to mujoco bridge
+    TOPIC_CONTROL = "tron1_linefoot_control".upper() # control from MATLAB
+
+    DEFAULT_JOINT_OFFSETS = np.array([0, 0.53 - 0.06, -0.55 - 0.54, 0,  # right leg
+                                    0, 0.53 - 0.06, -0.55 - 0.54, 0]) # left leg
+
+    DEFAULT_JOINT_POS = np.array([0.0, 0.25, -0.5, 0, 
+                                0.0, 0.25, -0.5, 0])
+
+    JOINT_IDX_2_MOTOR_IDX = np.array([4, 5, 6, 7,
+                                    0, 1, 2, 3])
+
+    MOTOR_POLARITY = np.array([1,  1, -1, -1,
+                            1, -1,  1, -1])
+else:
+    raise ValueError("Unsupported robot type!")
 
 
 class RobotReceiver:
@@ -58,52 +93,50 @@ class RobotReceiver:
               "\n  message: " + diagnostic_value.message)
 
     def lowCmdHandler(self, channel, data):
-        self.low_cmd = tron1_pointfoot_control_t.decode(data)
+        self.low_cmd = eval(TOPIC_CONTROL.lower() + "_t").decode(data)
 
     def lcmHandleThread(self):
         while self.is_running:
             lc.handle_timeout(0)
 
-def sdk_state_to_lcm_state(robot_state: datatypes.RobotState, low_state: tron1_pointfoot_state_t):
-    low_state.qj_pos[:3] = robot_state.q[3:]
-    low_state.qj_pos[3:] = robot_state.q[:3]
-    low_state.qj_pos[1] *=-1
-    low_state.qj_pos[5] *=-1
-    low_state.qj_vel[:3] = robot_state.dq[3:]
-    low_state.qj_vel[3:] = robot_state.dq[:3]
-    low_state.qj_vel[1] *=-1
-    low_state.qj_vel[5] *=-1
-    low_state.qj_tau[:3] = robot_state.tau[3:]
-    low_state.qj_tau[3:] = robot_state.tau[:3]
-    low_state.qj_tau[1] *=-1
-    low_state.qj_tau[5] *=-1
+def sdk_state_to_lcm_state(sdk_state: datatypes.RobotState, lcm_state):
+    for idx in range(len(lcm_state.qj_pos)):
+        motor_idx = JOINT_IDX_2_MOTOR_IDX[idx]
+        lcm_state.qj_pos[idx] = sdk_state.q[motor_idx] * MOTOR_POLARITY[motor_idx]
+        lcm_state.qj_vel[idx] = sdk_state.dq[motor_idx] * MOTOR_POLARITY[motor_idx]
+        lcm_state.qj_tau[idx] = sdk_state.tau[motor_idx] * MOTOR_POLARITY[motor_idx]
 
-def lcm_cmd_to_sdk_cmd(low_cmd: tron1_pointfoot_control_t, robot_cmd: datatypes.RobotCmd):
-    robot_cmd.mode = [0.0 for _ in range(motor_number)]
-    qj_pos_lower = (np.array((low_cmd.qj_pos)) - JOINT_OFFSETS_HIGH).tolist()
-    robot_cmd.q[:3] = qj_pos_lower[3:]
-    robot_cmd.q[3:] = qj_pos_lower[:3]
-    robot_cmd.q[2] *=-1
-    robot_cmd.q[4] *=-1
-    robot_cmd.dq[:3] = low_cmd.qj_vel[3:]
-    robot_cmd.dq[3:] = low_cmd.qj_vel[:3]
-    robot_cmd.dq[2] *=-1
-    robot_cmd.dq[4] *=-1
-    robot_cmd.tau[:3] = low_cmd.qj_tau[3:]
-    robot_cmd.tau[3:] = low_cmd.qj_tau[:3]
-    robot_cmd.tau[2] *=-1
-    robot_cmd.tau[4] *=-1
-    robot_cmd.Kp[:3] = low_cmd.kp[3:]
-    robot_cmd.Kp[3:] = low_cmd.kp[:3]
-    robot_cmd.Kd[:3] = low_cmd.kd[3:]
-    robot_cmd.Kd[3:] = low_cmd.kd[:3]
+
+def lcm_cmd_to_sdk_cmd(lcm_cmd, sdk_cmd: datatypes.RobotCmd):
+    sdk_cmd.mode = [0.0 for _ in range(motor_number)] # torque mode
+    sdk_cmd.q = [0.0 for _ in range(motor_number)]
+    sdk_cmd.dq = [0.0 for _ in range(motor_number)]
+    sdk_cmd.tau = [0.0 for _ in range(motor_number)]
+    sdk_cmd.Kp = [0.0 for _ in range(motor_number)]
+    sdk_cmd.Kd = [0.0 for _ in range(motor_number)]
+
+    for idx in range(len(lcm_cmd.qj_pos)):
+        motor_idx = JOINT_IDX_2_MOTOR_IDX[idx]
+        sdk_cmd.q[motor_idx] = (lcm_cmd.qj_pos[idx] - DEFAULT_JOINT_OFFSETS[idx]) * MOTOR_POLARITY[motor_idx]
+        sdk_cmd.dq[motor_idx] = lcm_cmd.qj_vel[idx] * MOTOR_POLARITY[motor_idx]
+        sdk_cmd.tau[motor_idx] = lcm_cmd.qj_tau[idx] * MOTOR_POLARITY[motor_idx]
+        sdk_cmd.Kp[motor_idx] = lcm_cmd.kp[idx]
+        sdk_cmd.Kd[motor_idx] = lcm_cmd.kd[idx]
 
 if __name__ == '__main__':
-    os.system("export ROBOT_TYPE=PF_TRON1A")
+    if ROBOT_TYPE == "PF":
+        # export ROBOT_TYPE=PF_TRON1A
+        os.environ["ROBOT_TYPE"] = "PF_TRON1A"
+    elif ROBOT_TYPE == "LF":
+        # export ROBOT_TYPE=SF_TRON1A
+        os.environ["ROBOT_TYPE"] = "SF_TRON1A"
+    else:
+        raise ValueError("Unsupported robot type!")
 
-    # Create a Robot instance of type PointFoot
+    os.system("echo $ROBOT_TYPE")
+
+    # Create a Robot instance
     robot = Robot(RobotType.PointFoot)
-
     robot_ip = "10.192.1.2"
 
     # Initialize the robot with robot_ip
@@ -112,7 +145,8 @@ if __name__ == '__main__':
 
     # Get motor number information
     motor_number = robot.getMotorNumber()
-    print(f"motor number: {motor_number}")
+    print(f"Motor number: {motor_number}")
+    assert motor_number == len(JOINT_IDX_2_MOTOR_IDX)
 
     # Create an instance of RobotReceiver to handle callbacks
     receiver = RobotReceiver()
@@ -132,22 +166,17 @@ if __name__ == '__main__':
     # Main loop to continuously publish robot commands
     rate = Rate(1000) # 1000 Hz
 
-
+    # Initialize LCM
     lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
-    low_cmd_suber = lc.subscribe("TRON1_CONTROL", receiver.lowCmdHandler)
+    low_cmd_suber = lc.subscribe(TOPIC_CONTROL, receiver.lowCmdHandler)
     low_cmd_suber.set_queue_capacity(1)
 
     lc_thread = Thread(target=receiver.lcmHandleThread, daemon=True)
     lc_thread.start()
-    
-    topic_state = "tron1_pointfoot_state" # state to mujoco bridge
-    topic_control = "TRON1_CONTROL" # control from MATLAB
-    polarity = [1, -1,  1,
-                1,  1, -1]
 
     cmd_msg = datatypes.RobotCmd()
     # Set default values for control mode, joint positions, velocities, torques, Kp, and Kd
-        # motor mode：0: torque； 1：speed； 2：position; default：0
+    # motor mode：0: torque； 1：speed； 2：position; default：0
     cmd_msg.mode = [2.0 for _ in range(motor_number)]
     cmd_msg.q = [0.0 for _ in range(motor_number)]
     cmd_msg.dq = [0.0 for _ in range(motor_number)]
@@ -158,22 +187,22 @@ if __name__ == '__main__':
     # Wait for the robot to initialize
     while receiver.robot_state is None or receiver.imu is None:
         time.sleep(0.1)
-        pass
 
     # Compensate the initial yaw
     quat_init = Quaternion(*receiver.imu.quat)
     rpy_init = quat_to_rpy(quat_init)
     yaw_init = rpy_init[2]
-    print("Initial yaw: ", yaw_init)
+    print(f"Initial yaw: {yaw_init}")
 
-    # TODO Compensate the initial acceleration
     acc_init = np.array(receiver.imu.acc)
-    print("Initial acceleration: ", acc_init)
-
+    print(f"Initial acceleration: {acc_init}")
 
     q_init = np.array(receiver.robot_state.q)
     # q_target = np.zeros(motor_number)
-    q_target = np.array([0.0, 0.25, 0.5, 0.0, -0.25, -0.5])
+    q_target = DEFAULT_JOINT_POS[JOINT_IDX_2_MOTOR_IDX] * MOTOR_POLARITY
+    print(f"q_init: {q_init}")
+    print(f"q_target: {q_target}")
+
     num_init_steps = 4000
     print("=> Enter initial pose...")
     for step in range(num_init_steps):
@@ -185,10 +214,10 @@ if __name__ == '__main__':
         robot.publishRobotCmd(cmd_msg)  # Publish the robot command
         rate.sleep()
 
-    imu_acc_filter = MovingWindowFilter(window_size=10, dim=3)
-    imu_gyro_filter = MovingWindowFilter(window_size=10, dim=3)
+    imu_acc_filter = MovingWindowFilter(window_size=3, dim=3)
+    imu_gyro_filter = MovingWindowFilter(window_size=3, dim=3)
 
-    print("=> Start to publish robot command...")
+    print("=> Start to publish robot state...")
 
     # Capture Ctrl+C signal
     signal.signal(signal.SIGINT, signal.default_int_handler)
@@ -201,14 +230,13 @@ if __name__ == '__main__':
                 cmd_msg = datatypes.RobotCmd()
                 lcm_cmd_to_sdk_cmd(low_cmd, cmd_msg)
                 cmd_msg.stamp = time.time_ns()
-                tau_wbc = np.array(cmd_msg.tau).clip(-max_tau, max_tau) / 2
+                tau_wbc = np.array(cmd_msg.tau).clip(-max_tau, max_tau)
                 # tau_wbc[[0, 1, 3, 4]] = 0
                 cmd_msg.tau = tau_wbc.tolist()
                 robot.publishRobotCmd(cmd_msg)  # Publish the robot command
-                low_cmd = None
 
             # Parse the received sensor readings
-            low_state = tron1_pointfoot_state_t()
+            low_state = eval(TOPIC_STATE + "_t")()
             if receiver.robot_state is not None:
                 sdk_state_to_lcm_state(receiver.robot_state, low_state)
 
@@ -222,10 +250,10 @@ if __name__ == '__main__':
                 low_state.rpy[2] = rpy[2] - yaw_init
                 low_state.quaternion = rpy_to_quat(low_state.rpy).to_numpy().tolist()
 
-
-            lc.publish(topic_state, low_state.encode())
+            lc.publish(TOPIC_STATE, low_state.encode())
             rate.sleep()  # Control loop frequency
     except:
+        traceback.print_exc()
         cmd_msg = datatypes.RobotCmd()
         cmd_msg.stamp = time.time_ns()  # Set the timestamp
         # Set default values for control mode, joint positions, velocities, torques, Kp, and Kd
